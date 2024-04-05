e292c7800862ea121830f511984c2b91b60b47a7
Akhileshait <akhileshpatidar989368@gmail.com>
2024-04-05 12:03:04 +0530

    hello

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ class song{
      }
 }
 
-const a = new song("Song 1",  "Genre 1", "theme1" ,"Artist 1",);
-const b = new song("Song 2", "Genre 2","theme2", "Artist 2", );
-const c = new song("Song 3", "Genre 3","theme3" ,"Artist 3");
+const a = new song("Song 1",  2, 5 ,"Artist 1",);
+const b = new song("Song 2", 1,6, "Artist 2", );
+const c = new song("Song 3", 1,10 ,"Artist 3");
 
 const songs= [a, b, c];
+
+const result=[]
 
 function RecentlyPlayed() {
      const recentlyPlayedList = document.getElementById("recentlyPlayed");
@@ -22,13 +24,11 @@ function RecentlyPlayed() {
           });
  }
 
- 
-
 function Artists() {
      const artistsList = document.getElementById("artists");
      songs.forEach(song => {
               const li = document.createElement("li");
-              li.textContent = song.artist;
+              li.textContent = song.artist + "";
               artistsList.appendChild(li);
      });
  }
@@ -42,6 +42,48 @@ function Genres() {
      });
 }
 
+function recommend(songs) {
+    const N = 1e5 + 10;
+    const vis = Array(N).fill(false);
+    const g = Array(N).fill().map(() => []);
+    const result = [];
+
+    function dfs(vertex) {
+        for (const child of g[vertex]) {
+            result.push([child[0], child[1]]);
+        }
+    }
+
+    // Preprocess the songs
+    songs.forEach(song => {
+        g[song.genre].push([song.potd, song.name]);
+    });
+
+    const history = [
+        { name: "song1", genre: 1, theme: 1, potd: 6 },
+        { name: "song2", genre: 2, theme: 2, potd: 7 }
+    ];
+
+    for (const i of history) {
+        if (!vis[i.genre]) {
+            dfs(i.genre);
+        }
+        vis[i.genre] = true;
+    }
+    result.sort((a, b) => b[0] - a[0]);
+    for (const item of result) {
+        const recsong = document.getElementById("recommended");
+        const li = document.createElement("li");
+        li.textContent = item[1];
+        recsong.appendChild(li);
+    }
+}
+
+
+recommend(songs);
+
+
+resultsong();
 RecentlyPlayed();
 Artists();
 Genres();
