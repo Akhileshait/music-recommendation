fa6a4edcdf8f76836b8ad814ab393ee4cda4d7b9
Akhileshait <akhileshpatidar989368@gmail.com>
2024-04-05 14:19:10 +0530

    added project files

--- Backend/src/routes/song.js
+++ Backend/src/routes/song.js
@@ -2,6 +2,7 @@ import express from "express";
 
 const router = express.Router();
 import { SongModel } from "../models/Song.js";
+import { UserModel } from "../models/User.js";
 
 router.post("/newSong", async (req, res) => {
   try {
@@ -21,12 +22,71 @@ router.post("/newSong", async (req, res) => {
 });
 
 router.get("/allSongs", async (req, res) => {
-    try {
-        const songs = await SongModel.find({});
-        return res.status(200).json(songs);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
+  try {
+    const songs = await SongModel.find({});
+    return res.status(200).json(songs);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.get("/artists", async (req, res) => {
+  try {
+    const _id = "660f83a64a9c29b0e10bcadd";
+    const user = await UserModel.findById(_id);
+    const artist = user.favArtist;
+    return res.status(200).json(artist);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.get("/genre", async (req, res) => {
+  try {
+    const _id = "660f83a64a9c29b0e10bcadd";
+    const user = await UserModel.findById(_id);
+    const genre = user.genre;
+    return res.status(200).json(genre);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.get("/history", async (req, res) => {
+  try {
+    const _id = "660f83a64a9c29b0e10bcadd";
+    const user = await UserModel.findById(_id);
+    const history = user.history;
+    const songs = [];
+    for (const historyId of history) {
+      const song = await SongModel.findById(historyId); // Assuming SongModel is your Mongoose model for songs
+      if (song) {
+        songs.push(song);
+      }
     }
-  })
+    return res.status(200).json(songs);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+
+router.get("/history1", async (req, res) => {
+  try {
+    const _id = "660f83a64a9c29b0e10bcadd";
+    const user = await UserModel.findById(_id);
+    const history = user.history;
+    const songs = [];
+    for (const historyId of history) {
+      const song = await SongModel.findById(historyId); // Assuming SongModel is your Mongoose model for songs
+      if (song) {
+        songs.push(song);
+      }
+    }
+    return res.status(200).json(songs);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 export { router as SongRouter };
